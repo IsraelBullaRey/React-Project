ae644448ae8019082e046c2ec2427a33481373fb
Israel <israelbr@ufps.edu.co>
2025-10-21 16:58:42 -0500

    Lógicacompelta

--- src/app/effect/page.js
+++ src/app/effect/page.js
@@ -14,11 +14,11 @@ export default function SimpsonsPage() {
     //   }
 
     function adelante() {
-        setIndex(index+1);
+        setIndex(index==1182 ? 1 : index+1);
     }
 
     function atras() {
-        setIndex(index==1 ? 1 : index-1);
+        setIndex(index==1 ? 1182 : index-1);
     }
 
     return (
@@ -32,17 +32,31 @@ export default function SimpsonsPage() {
         <input
             type="number"
             min="1"
-            max="10"
+            max="1182"
             className="border text-xl bg-white mt-2 mx-10 rounded-lg"
+            value={index} //convierte al <input> en un componente controlado
             onChange={(e) => {
-            setIndex(e === "" ? 1 : parseInt(e.target.value, 10));
+                const val = parseInt(e.target.value, 10);
+                if(isNaN(val)) { //Si está vacío (no es número) lo posiciona a 1
+                    setIndex(1);
+                }else{
+                    //Si no está vacío entonces lo limita entre 1 y 1182
+                    //que es la cantidad maxima de personajes.
+                    const limite = Math.min(1182, Math.max(1, val)); 
+                    //Math.max garantiza que el valor nunca sea menor que 1, si val es -10, el mayor entre -10 y 1 es 1.
+                    //Math.min garantiza que el valor no exceda de 1182, si val es 2000, math.max lo deja en 2000 ya que es mayor a 1
+                    //Más en el Math.min se seleccionará 1182 ya que es el minimo entre 2000 y 1182.
+                    setIndex(limite);
+                }
             }}
         />
         <div className="flex flex-col items-center justify-center">
-            <button className="m-2 py-5 bg-indigo-300 rounded-lg hover:bg-indigo-400 hover:shadow-lg text-lg font-bold text-white w-40" onClick={atras}>
+            <button className="m-2 py-5 bg-indigo-300 rounded-lg hover:bg-indigo-400 hover:shadow-lg text-lg font-bold text-white w-40" 
+                    onClick={atras}>
                 ⬅️ Atrás
             </button>
-            <button className="m-2 py-5 bg-indigo-300 rounded-lg hover:bg-indigo-400 hover:shadow-lg text-lg font-bold text-white w-40" onClick={adelante}>
+            <button className="m-2 py-5 bg-indigo-300 rounded-lg hover:bg-indigo-400 hover:shadow-lg text-lg font-bold text-white w-40" 
+                    onClick={adelante}>
                 Adelante ➡️
             </button>
         </div>
